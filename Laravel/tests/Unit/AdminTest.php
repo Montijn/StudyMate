7a1b5178c054ad6338f383e7a17d4340e230a8e6
Montijn <ms.bennik@student.avans.nl>
2020-06-10 22:14:52 +0200

    Werkende test new user

--- Laravel/tests/Unit/AdminTest.php
+++ Laravel/tests/Unit/AdminTest.php
@@ -3,15 +3,16 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-
+use Illuminate\Foundation\Testing\RefreshDatabase;
 class AdminTest extends TestCase
 {
-
+    use RefreshDatabase;
     /** @test */
     public function GuestUnauthorizedTest()
     {
-
-        $response = $this->get('/teacheroverview');
-        $response->assertStatus(500);
+        $new_User = factory(\App\User::class)->create();
+        $this->assertDatabaseHas('Users', [
+            'firstname' => 'Montijn',
+        ]);
     }
 }
