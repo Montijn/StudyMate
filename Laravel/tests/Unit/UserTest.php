87942466136f25fd88a524cbd0026f750f9c01f8
Montijn <ms.bennik@student.avans.nl>
2020-06-11 15:02:40 +0200

    Update UserTest.php

--- Laravel/tests/Unit/UserTest.php
+++ Laravel/tests/Unit/UserTest.php
@@ -23,26 +23,47 @@ class UserTest extends TestCase
     /** @test */
     public function AdminUnauthorizedTest(){
         // 1. Arrange
-        $Admin = factory(\App\User::class)->create();
+        $admin = factory(\App\User::class)->create();
+        auth()->login($admin);
         // 2. Act
-        $bool = Gate::forUser($Admin)->denies('CheckStudent');
+        $bool = Gate::forUser($admin)->denies('CheckStudent');
         // 3. Assert
         $this->assertEquals(true, $bool);
     }
     /** @test */
     public function StudentUnauthorizedTest(){
         // 1. Arrange
-        $Student = new User([
+        $student = new User([
             'firstname' => 'Montijn',
             'lastname' => 'Bennik',
             'email' => 'ms.bennik@student.avans.nl',
             'role' => 'Student',
             'password' => Hash::make('test1234')
         ]);
+        auth()->login($student);
 
         // 2. Act
-        $bool = Gate::forUser($Student)->denies('CheckAdmin');
+        $bool = Gate::forUser($student)->denies('CheckAdmin');
         // 3. Assert
         $this->assertEquals(true, $bool);
     }
+    /** @test */
+    public function GuestStudentUnauthorizedTest(){
+        // 1. Arrange
+        $admin = factory(\App\User::class)->create();
+        // 2. Act
+        $bool = Gate::forUser($admin)->denies('CheckStudent');
+        // 3. Assert
+        $this->assertEquals(true, $bool);
+    }
+    /** @test */
+    public function GuestAdminUnauthorizedTest(){
+        // 1. Arrange
+        $admin = factory(\App\User::class)->create();
+        // 2. Act
+        $bool = Gate::forUser($admin)->denies('CheckAdmin');
+        // 3. Assert
+        $this->assertEquals(true, $bool);
+    }
+
 }
