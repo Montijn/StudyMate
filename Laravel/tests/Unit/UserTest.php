1be660b1f33c4df18d5e916e0bf067350009553a
Montijn <ms.bennik@student.avans.nl>
2020-06-11 13:16:36 +0200

    UnauthorizedTest

--- Laravel/tests/Unit/UserTest.php
+++ Laravel/tests/Unit/UserTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Unit;
 
+use App\User;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Gate;
 class UserTest extends TestCase
 {
     use RefreshDatabase;
@@ -14,5 +17,30 @@ class UserTest extends TestCase
         $this->assertDatabaseHas('Users', [
             'firstname' => 'Montijn',
         ]);
+    }
+    /** @test */
+    public function AdminUnauthorizedTest(){
+        // 1. Arrange
+        $Admin = factory(\App\User::class)->create();
+        // 2. Act
+        $bool = Gate::forUser($Admin)->denies('CheckStudent');
+        // 3. Assert
+        $this->assertEquals(true, $bool);
+    }
+    /** @test */
+    public function StudentUnauthorizedTest(){
+        // 1. Arrange
+        $Student = new User([
+            'firstname' => 'Montijn',
+            'lastname' => 'Bennik',
+            'email' => 'ms.bennik@student.avans.nl',
+            'role' => 'Student',
+            'password' => Hash::make('test1234')
+        ]);
+
+        // 2. Act
+        $bool = Gate::forUser($Student)->denies('CheckAdmin');
+        // 3. Assert
+        $this->assertEquals(true, $bool);
     }
 }
